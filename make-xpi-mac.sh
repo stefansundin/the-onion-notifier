c450b3ee053ecb29668ba8061368e0073e65023f
Stefan Sundin <git@stefansundin.com>
2017-08-15 14:09:45 -0700

    New bash script using jq.

--- make-xpi-mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-V=$(cat extension/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
-rm -f "the-onion-notifier-$V.xpi"
-cd extension
-zip -r "../the-onion-notifier-$V.xpi" . -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db'
--- make-xpi.sh
+++ make-xpi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-V=$(cat extension/manifest.json | grep '"version"' | grep -oP "\d+\.\d+\.\d+")
+V=$(cat extension/manifest.json | jq -Mr .version)
 rm -f "the-onion-notifier-$V.xpi"
 cd extension
 zip -r "../the-onion-notifier-$V.xpi" . -x '*.git*' -x '*.DS_Store' -x '*Thumbs.db'
